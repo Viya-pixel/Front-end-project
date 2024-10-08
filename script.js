7e3d69eec3b331c666a4217aa6183196747be0c0
Viya-pixel <vp0801892@gmail.com>
2024-10-08 23:13:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("bookingForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Your bike has been successfully booked with BikeWaala!");
+});
